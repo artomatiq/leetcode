94c585195905208e58f27d45c792cd62eb46657f
Art Vardanyan <akvardanyan99@gmail.com>
2025-05-15 14:09:05 -0400

    1. Two Sum

--- TopInterview150/1.easy.js
+++ TopInterview150/1.easy.js
@@ -1,61 +1,48 @@
 /*
-    88. Merge Sorted Array
+1. Two Sum
 
-    Easy
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
-
-
+You can return the answer in any order.
 
 Example 1:
 
-Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-Output: [1,2,2,3,5,6]
-Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 Example 2:
 
-Input: nums1 = [1], m = 1, nums2 = [], n = 0
-Output: [1]
-Explanation: The arrays we are merging are [1] and [].
-The result of the merge is [1].
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
 Example 3:
 
-Input: nums1 = [0], m = 0, nums2 = [1], n = 1
-Output: [1]
-Explanation: The arrays we are merging are [] and [1].
-The result of the merge is [1].
-Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
-
+Input: nums = [3,3], target = 6
+Output: [0,1]
 
 Constraints:
 
-nums1.length == m + n
-nums2.length == n
-0 <= m, n <= 200
-1 <= m + n <= 200
--109 <= nums1[i], nums2[j] <= 109
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
 
-
-Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 
-
 /**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  */
-var merge = function(nums1, m, nums2, n) {
-    if(!n) return
-    for (let i=m; i<m+n; i++) {
-        nums1[i] = nums2[i-m]
+var twoSum = function(nums, target) {
+    let map = new Map()
+    for (i=0; i<nums.length; i++) {
+        const int = nums[i]
+        if (map.has(int)) {
+            return [map.get(int), i]
+        }
+        map.set(target-int, i)
     }
-    nums1.sort((a, b) => a - b)
 };
