5e391b0a83a189f2374adb61cde895881b39266e
Art Vardanyan <akvardanyan99@gmail.com>
2025-05-20 09:55:46 -0400

    21.easy draft sln

--- TopInterview150/21.easy.MergeTwoSortedLists.js
+++ TopInterview150/21.easy.MergeTwoSortedLists.js
@@ -30,3 +30,81 @@ The number of nodes in both lists is in the range [0, 50].
 
 Both list1 and list2 are sorted in non-decreasing order.
  */
+
+//initial solution
+
+var mergeTwoLists = function (list1, list2) {
+    class List {
+        constructor() {
+            this.head = null
+            this.tail = null
+            this.size = 0
+        }
+
+        enqueue(val) {
+            const newNode = {val: val, next: null}
+            if (!this.head) {
+                this.head = newNode
+                this.tail = newNode
+                this.size++
+                console.log('enqueued: ', this.head.val)
+            } else {
+                this.tail.next = newNode
+                this.tail = newNode
+                this.size++
+                console.log('enqueued: ', this.head.val)
+            }
+        }
+
+        dequeue() {
+            if (!this.head) {
+                console.log('queue is empty')
+                return null
+            } else {
+                const dequeuedNode = this.head.val
+                this.head = this.head.next
+                this.size--
+                console.log('dequeued: ', dequeuedNode.val)
+                return dequeuedNode.val
+            }
+        }
+
+        print() {
+            if (!this.head) console.log ('list is empty')
+            else {
+                let cur = this.head
+                let array = []
+                while (cur) {
+                    array.push(cur.val)
+                    cur = cur.next
+                }
+                console.log(array)
+            }
+        }
+    }
+    const list = new List()
+    let val1 = list1
+    let val2 = list2
+
+    while (val1 && val2) {
+        if (val1.val < val2.val) {
+            list.enqueue(val1.val)
+            val1 = val1.next
+        } else {
+            list.enqueue(val2.val)
+            val2 = val2.next
+        }
+    }
+    if (val1) {
+        while (val1) {
+            list.enqueue(val1.val)
+            val1 = val1.next
+        }
+    } else if (val2) {
+        while (val2) {
+            list.enqueue(val2.val)
+            val2 = val2.next
+        }
+    }
+    return list.head
+};
