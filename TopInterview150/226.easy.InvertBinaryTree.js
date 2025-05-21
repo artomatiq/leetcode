bc15f9074dcee9c36626f061658ae23d50700cb6
Art Vardanyan <akvardanyan99@gmail.com>
2025-05-21 09:43:35 -0400

    226 - breadth first

--- TopInterview150/226.easy.InvertBinaryTree.js
+++ TopInterview150/226.easy.InvertBinaryTree.js
@@ -24,24 +24,50 @@ The number of nodes in the tree is in the range [0, 100].
 
 //here is the recursive solution
 
-var invertTree = function(root) {
+var invertTree = function (root) {
     if (!root) return root
 
-    function swap (triangle) {
+    function swap(triangle) {
         const dummy = triangle.left
         triangle.left = triangle.right
         triangle.right = dummy
         return triangle
     }
 
-    function reverse (tree) {
+    function reverse(tree) {
         if (!(tree.left || tree.right)) return tree
-        
+
         if (tree.left) reverse(tree.left)
         if (tree.right) reverse(tree.right)
-        
+
         return swap(tree)
     }
     return reverse(root)
+}
+
+//here are the iterative solutions
+
+//BFS - breadth first
+
+var invertTree = function (root) {
+    if (!root) return root
+    let stack = [root]
+
+    function swap(node) {
+        const temp = node.left
+        node.left = node.right
+        node.right = temp
+    }
+    while (stack.length) {
+        const cur = stack.pop()
+        swap(cur)
+        if (cur.left) stack.push(cur.left)
+        if (cur.right) stack.push(cur.right)
+    }
+    return root
 } 
+
+
+
+//DPS - depth first
 
