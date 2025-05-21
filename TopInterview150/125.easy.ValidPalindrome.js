07b78ff02759772a36ca9c4bff01cda6196f23fa
Art Vardanyan <akvardanyan99@gmail.com>
2025-05-21 07:26:30 -0400

    125.done

--- TopInterview150/125.easy.ValidPalindrome.js
+++ TopInterview150/125.easy.ValidPalindrome.js
@@ -49,3 +49,55 @@ var isPalindrome = function(s) {
     }
     return string === string.split('').reverse().join('')
 };
+
+
+/**
+ * the string1 === string2 has a time complexity of O(n) in the worst case
+ * it is not optimal as far as performance is concerned
+ * 
+ * if using an extra string can be avoided, we will achieve better space complexity
+ * 
+ * string concatonations are costly....since strings are immutable
+ *      JS creates a new string
+ *      copies old string
+ *      appends new parts
+ */
+
+
+//this is a much more efficient solution
+
+var isPalindrome = function(s) {
+    
+    function isAlphaNum (char) {
+        const charCode = char.charCodeAt(0)
+        if (charCode >=48 && charCode <= 57) {
+            return char
+        }
+        else if (charCode >=97 && charCode <= 122) {
+            return char
+        }
+        else if (charCode >=65 && charCode <= 90) {
+            return String.fromCharCode(charCode+32)
+        }
+        return false
+    }
+
+    let left = 0
+    let right = s.length - 1
+
+    while (left<right) {
+        const newLeft = isAlphaNum(s[left])
+        const newRight = isAlphaNum(s[right])
+        if (!newLeft) {
+            left++
+            continue
+        } else if (!newRight) {
+            right--
+            continue
+        }
+        if (newLeft !== newRight) return false
+        left++
+        right--
+    }
+    return true
+};
