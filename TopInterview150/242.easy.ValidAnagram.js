62222953e86210d6122e5cfb4b5ec92f338c84f0
Art Vardanyan <akvardanyan99@gmail.com>
2025-05-22 07:54:52 -0400

    solved

--- TopInterview150/242.easy.ValidAnagram.js
+++ TopInterview150/242.easy.ValidAnagram.js
@@ -24,3 +24,22 @@ s and t consist of lowercase English letters.
 
 Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
  */
+
+
+//my initial solution
+
+var isAnagram = function(s, t) {
+    const map = new Map()
+    s.split('').forEach(char => {
+        map.set(char, (map.get(char) || 0) + 1)
+    })
+    for (char of t.split('')) {
+        if (!map.has(char)) {
+            return false
+        } else if (map.get(char) === 1) {
+            map.delete(char)
+        } else map.set(char, map.get(char) - 1)
+    }
+    return !map.size
+};
+
