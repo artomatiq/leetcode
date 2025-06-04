59f6ad3085dfeb0e767e095bf1adc1cf24a73e5d
Art Vardanyan <akvardanyan99@gmail.com>
2025-06-04 10:36:28 -0400

    206.easy-cleaner sln

--- TopInterview150/206.easy.ReverseLinkedList.js
+++ TopInterview150/206.easy.ReverseLinkedList.js
@@ -45,3 +45,17 @@ var reverseList = function(head) {
     head = b
     return head
 };
+
+//simplified
+
+var reverseList = function(head) {
+    let prev = null
+    let cur = head
+    while (cur) {
+        let temp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = temp
+    }
+    return prev
+};
