74f103ae177db2d0584ccf6a51cbbe7f9ef5e8e7
Art Vardanyan <akvardanyan99@gmail.com>
2025-07-01 13:36:45 -0400

    67.optimal sln

--- TopInterview150/67.easy.AddBinary.js
+++ TopInterview150/67.easy.AddBinary.js
@@ -31,3 +31,28 @@ var addBinary = function(a, b) {
 };
 
 //this does not work because parseInt() is imprecise for very large inputs
+
+
+//optimal solution
+
+var addBinary = function(a, b) {
+    let result = '';
+    let carry = 0;
+
+    let i = a.length - 1;
+    let j = b.length - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+        const bitA = i >= 0 ? parseInt(a[i]) : 0;
+        const bitB = j >= 0 ? parseInt(b[j]) : 0;
+
+        const sum = bitA + bitB + carry;
+        result = (sum % 2) + result;
+        carry = Math.floor(sum / 2);
+
+        i--;
+        j--;
+    }
+
+    return result;
+};
