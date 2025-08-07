6370bf4047f26fb33e1e8d092f888636a7fece06
Art Vardanyan <akvardanyan99@gmail.com>
2025-08-07 15:01:20 -0400

    217.easy---sln with Set()

--- TopInterview150/217.easy.ContainsDuplicate.js
+++ TopInterview150/217.easy.ContainsDuplicate.js
@@ -30,3 +30,18 @@ Constraints:
 -109 <= nums[i] <= 109
  */
 
+//my sln using a Set()
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set()
+    for(let num of nums) {
+        if (set.has(num)) return true
+        else set.add(num)
+    }
+    return false
+};
+
