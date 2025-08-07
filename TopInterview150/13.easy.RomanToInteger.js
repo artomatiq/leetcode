6dd730eb9f32b74907aeef20d529dc4c84f3bc15
Art Vardanyan <akvardanyan99@gmail.com>
2025-08-07 17:29:04 -0400

    13.easy - optimized

--- TopInterview150/13.easy.RomanToInteger.js
+++ TopInterview150/13.easy.RomanToInteger.js
@@ -77,3 +77,29 @@ var romanToInt = function(s) {
     }
     return result
 };
+
+//
+
+var romanToInt = function (s) {
+    const map = new Map([
+        ['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]
+    ])
+    const specials = {
+        'IV': 4,
+        'IX': 9,
+        'XL': 40,
+        'XC': 90,
+        'CD': 400,
+        'CM': 900,
+    }
+    let result = 0
+    for (let i = 0; i < s.length; i++) {
+        if (map.get(s[i]) < map.get(s[i+1])) {
+            result += specials[`${s[i]}${s[i+1]}`]
+            i++
+        } else {
+            result += map.get(s[i])
+        }
+    }
+    return result
+};
